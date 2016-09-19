cb25468377acaead7f5de4bb73e404dc3f94f386
Jerica Mitchell <jericam32@gmail.com>
2016-09-18 23:22:59 -0500

    Add files via upload

---
+++ portfolio-master/assets/js/basic.js
@@ -0,0 +1,22 @@
+
+$(function(){
+    $("#tada").textillate({ in: { effect: 'tada' }  });
+});
+
+$(function(){
+    $(".bounceIn").textillate({ in: { effect: 'bounceInDown' } });
+});
+
+$(function(){
+	$(".fadeRight").textillate({ in: { effect: 'fadeInBigLeft' } });
+});
+
+$(function(){
+	$(".bounceUp").textillate({ in: { effect: 'bounceInUp' } });
+});
+
+$(function(){
+    $(".flipIn").textillate({ in: { effect: 'flipInX' } });
+});
+
+
